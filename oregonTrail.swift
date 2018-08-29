788a754e70564a60b3780f595b8e46d105e327c4
alex watson <watsona11@go.lisd.net>
2018-08-28 19:02:58 -0500

    Update oregonTrail.swift

--- oregonTrail.swift
+++ oregonTrail.swift
@@ -5,20 +5,59 @@ print("Welcome")
 print("Oregon trail is a long and painful journey")
 print("You should probably get some supplies")
 print("Welcome to the KRABBY PATTY SUPER SHOP")
-print("You have $100.")
+realMoney = false
+while realMoney == false (){
+print("You have $250.")
 print("How many cows do you want, Each cow is 10")
 print("I recommend at least 3 ")
-var numberOfCowsPurchasedStr: String = readLine()!
-var numberOfCowsPurchasedInt: Int = Int(numberOfCowsPurchasedStr)!
-print("NUMBER OF COWS PURCHASED : " + numberOfCowsPurchasedStr)
-print("you have : ")
-print(100 - numberOfCowsPurchasedInt)
-print("dollars left")
-print("cows     = " + numberOfCowsPurchasedStr)
+var numberOfSnailsPurchasedStr: String = readLine()!
+var numberOfSnailsPurchasedInt: Int = Int(numberOfSnailsPurchasedStr)!
+print("NUMBER OF COWS PURCHASED : " + numberOfSnailsPurchasedStr)
+print("cows     = " + numberOfSnailsPurchasedInt)
 print("food     =   0")
 print("clothing =   0")
 print("ammo     =   0")
 print("parts    =   0")
 print("you have : ")
-print(100 - numberOfCowsPurchasedInt)
-print("dollars left") 
+spent = print(250 - numberOfSnailPurchasedInt)
+spent
+print("dollars left")
+print("How many krabbypattys do you want, Each krabbypatty is 10$")
+print("I recommend at least 25 ")
+var poundsOfFoodPurchasedStr: String = readLine()!
+var poundsOfFoodPurchasedInt: Int = Int(poundsOfFoodPurchasedStr)!
+print("NUMBER OF COWS PURCHASED : " + poundsOfFoodPurchasedStr)
+print("you have : ")
+spent = print(spent - poundsOfFoodPurchasedInt)
+spent
+print("dollars left")
+print("cows     = " + numberOfSnailsPurchasedStr)
+print("food     = " + poundsOfFoodPurchasedStr)
+print("clothing =   0")
+print("ammo     =   0")
+print("parts    =   0")
+print("you have : ")
+print("How many suspenders do you want, Each suspenders is 10")
+print("I recommend at least 25 ")
+var suspendersPurchasedStr: String = readLine()!
+var suspendersPurchasedInt: Int = Int(suspendersPurchasedStr)!
+print("NUMBER OF COWS PURCHASED : " + poundsOfFoodPurchasedStr)
+print("you have : ")
+spent = print(spent - poundsOfFoodPurchasedInt)
+spent
+print("dollars left")
+print("cows     = " + numberOfSnailsPurchasedStr)
+print("food     = " + poundsOfFoodPurchasedStr)
+print("clothing = " + suspendersPurchasedStr)
+print("ammo     = " )
+print("parts    =   0")
+print("you have : ")
+spent = print(250 - numberOfSnailPurchasedInt)
+spent
+print("dollars left")
+  if spent >= 0(){
+  print("Nice, Now to start on your journey!!!!!")
+
+  }
+}
+
