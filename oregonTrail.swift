1b25a5d1f2bb8df0023289d5d2a32e4636a1d44b
alex watson <watsona11@go.lisd.net>
2018-09-13 19:01:42 -0500

    Update oregonTrail.swift

--- oregonTrail.swift
+++ oregonTrail.swift
@@ -1,6 +1,5 @@
 // $ cd Documents/AppTeam/alex
 // $ swift oregon.swift
-
 import Foundation
 
 print("Welcome")
@@ -8,14 +7,23 @@ print("Oregon trail is a long and painful journey")
 print("You should probably get some supplies")
 print("Welcome to the KRABBY PATTY SUPER SHOP")
 var suppliesPurchased: Bool = false
+var numberOfSnailsPurchasedStr: String = readLine()!
+var numberOfSnailsPurchasedInt: Int = Int(numberOfSnailsPurchasedStr)!
+var poundsOfFoodPurchasedStr: String = readLine()!
+var poundsOfFoodPurchasedInt: Int = Int(poundsOfFoodPurchasedStr)!
+var suspendersPurchasedStr: String = readLine()!
+var suspendersPurchasedInt: Int = Int(suspendersPurchasedStr)!
+var jellyFishNetsPurchasedStr: String = readLine()!
+var jellyFishNetsPurchasedInt: Int = Int(jellyFishNetsPurchasedStr)!
+var boatPartsPurchasedStr: String = readLine()!
+var boatPartsPurchasedInt: Int = Int(boatPartsPurchasedStr)!
+
 
 var money: Int = 100
 while suppliesPurchased == false {
   print("You have $100.")
   print("How many snails do you want")
   print("I recommend at least 3 ")
-  var numberOfSnailsPurchasedStr: String = readLine()!
-  var numberOfSnailsPurchasedInt: Int = Int(numberOfSnailsPurchasedStr)!
   print("NUMBER OF SNAILS PURCHASED : " + numberOfSnailsPurchasedStr)
   print("snails   = " + numberOfSnailsPurchasedStr)
   print("food     =   0")
@@ -29,8 +37,6 @@ while suppliesPurchased == false {
   print("dollars left")
   print("How many krabbypattys do you want")
   print("I recommend at least 25 ")
-  var poundsOfFoodPurchasedStr: String = readLine()!
-  var poundsOfFoodPurchasedInt: Int = Int(poundsOfFoodPurchasedStr)!
   print("POUNDS OF FOOD PURCHASED : " + poundsOfFoodPurchasedStr)
   print("snails   = " + numberOfSnailsPurchasedStr)
   print("food     = " + poundsOfFoodPurchasedStr)
@@ -44,8 +50,6 @@ while suppliesPurchased == false {
 
   print("How many suspenders do you want")
   print("I recommend at least 15 ")
-  var suspendersPurchasedStr: String = readLine()!
-  var suspendersPurchasedInt: Int = Int(suspendersPurchasedStr)!
   print("NUMBER OF SUSPENDERS PURCHASED : " + suspendersPurchasedStr)
   print("snails   = " + numberOfSnailsPurchasedStr)
   print("food     = " + poundsOfFoodPurchasedStr)
@@ -59,8 +63,6 @@ while suppliesPurchased == false {
 
   print("How many jelly fish nets (ammo) do you want")
   print("I recommend at least 50 ")
-  var jellyFishNetsPurchasedStr: String = readLine()!
-  var jellyFishNetsPurchasedInt: Int = Int(jellyFishNetsPurchasedStr)!
   print("NUMBER OF NETS PURCHASED : " + jellyFishNetsPurchasedStr)
   print("snails   = " + numberOfSnailsPurchasedStr)
   print("food     = " + poundsOfFoodPurchasedStr)
@@ -74,8 +76,6 @@ while suppliesPurchased == false {
 
   print("How many boat parts do you want")
   print("I recommend to spend the rest of your money")
-  var boatPartsPurchasedStr: String = readLine()!
-  var boatPartsPurchasedInt: Int = Int(boatPartsPurchasedStr)!
   print("NUMBER OF NETS PURCHASED : " + boatPartsPurchasedStr)
   print("snails   = " + numberOfSnailsPurchasedStr)
   print("food     = " + poundsOfFoodPurchasedStr)
@@ -89,6 +89,8 @@ while suppliesPurchased == false {
 
   if money >= 0 && numberOfSnailsPurchasedInt >= 3 && poundsOfFoodPurchasedInt >= 15{
     print("Now time to start your journey")
+    print("40 days till you you get there Fisher")
+
     suppliesPurchased = true
   }
 
@@ -97,20 +99,58 @@ while suppliesPurchased == false {
   }
 }
 
-var alive: Bool = true
-var arrived: Bool = false
-while alive && arrived == false{
+
+
+var days: Int = 40
+let alive: Bool = true
+let arrived: Bool = false
+let counter: Int = 0
+while alive && arrived == false && days > 0{
   print ("do you want to hunt or continue?")
-  var choice: String = readLine()!
-  if  readLine()! == "hunt"{
-    //hunt
+  let choice: String = readLine()!
+  if  choice == "hunt" {
+    jellyFishNetsPurchasedInt = jellyFishNetsPurchasedInt - 5
     print("time for jellyfish hunting ")
-    var huntWord = Int(arc4random_uniform(3))
-    if huntWord == 1 {
-      print("hi")
-    }
-    }else{
-    //random sickness & decrease food
+    let hunter = Int(arc4random_uniform(2))
+    if hunter == 1 {
+      print("type hiya ")
+      let huntWord: String = readLine()!
+      if huntWord == "hiya" {
+        print("bulls eye straight in the eye of the net ")
+      }
+
+    } else {
+        print("type yarr ")
+        let huntWord: String = readLine()!
+          if huntWord == "yarr" {
+          print("bulls eye straight in the eye of the net ")
+        }
+      }
+  }else{
+      poundsOfFoodPurchasedInt = poundsOfFoodPurchasedInt - 5
+      days = days - 1
+      let sickness = Int(arc4random_uniform(3))
+      if sickness == 0 {
+        print("Mr.Star got sea sickness waste 2 days")
+        days = days + 2
+        poundsOfFoodPurchasedInt = poundsOfFoodPurchasedInt - 10
+        suspendersPurchasedInt = suspendersPurchasedInt - 2
+        print(days)
+	      print(" days till you get there old pall")
+
+      }
+      if sickness == 1{
+        print("pirates have raided you you lost some items")
+        let pirateRaid = Int(arc4random_uniform(1))
+        if pirateRaid == 0{
+          jellyFishNetsPurchasedInt = jellyFishNetsPurchasedInt - 5
+          boatPartsPurchasedInt = boatPartsPurchasedInt - 1
+        }
+        if pirateRaid == 1{
+          print("hi")
+        }
+      }
+
+
     }
   }
-
